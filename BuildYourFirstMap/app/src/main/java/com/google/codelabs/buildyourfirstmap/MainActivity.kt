ab25eee5bbfa025be34371f8dffe06e93c080531
David Mendoza <david.mendoza.mnez@gmail.com>
2021-02-06 20:35:06 +0100

    Google Maps Platform 101—Android: Enable and use the Maps SDK for Android to add Google Maps to the project. Add, customize, and cluster markers. Draw polylines and polygons on the map. Control the viewpoint of the camera programmatically

--- BuildYourFirstMap/app/src/main/java/com/google/codelabs/buildyourfirstmap/MainActivity.kt
+++ BuildYourFirstMap/app/src/main/java/com/google/codelabs/buildyourfirstmap/MainActivity.kt
@@ -14,7 +14,102 @@
 
 package com.google.codelabs.buildyourfirstmap
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.Circle
+import com.google.android.gms.maps.model.CircleOptions
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.codelabs.buildyourfirstmap.place.Place
+import com.google.codelabs.buildyourfirstmap.place.PlaceRenderer
+import com.google.codelabs.buildyourfirstmap.place.PlacesReader
+import com.google.maps.android.clustering.ClusterManager
 
 class MainActivity : AppCompatActivity() {
+
+    private val places: List<Place> by lazy {
+        PlacesReader(this).read()
+    }
+
+    private var circle: Circle? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.map_fragment) as? SupportMapFragment
+        mapFragment?.getMapAsync { googleMap ->
+            // Ensure all places are visible in the map.
+            googleMap.setOnMapLoadedCallback {
+                val bounds = LatLngBounds.builder()
+                places.forEach { bounds.include(it.latLng) }
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds.build(), 20))
+            }
+
+            addClusteredMarkers(googleMap)
+
+            // Set custom info window adapter
+            googleMap.setInfoWindowAdapter(MarkerInfoWindowAdapter(this))
+        }
+    }
+
+    /**
+     * Adds markers to the map with clustering support.
+     */
+    private fun addClusteredMarkers(googleMap: GoogleMap) {
+        // Create the ClusterManager class and set the custom renderer.
+        val clusterManager = ClusterManager<Place>(this, googleMap)
+        clusterManager.renderer =
+            PlaceRenderer(
+                this,
+                googleMap,
+                clusterManager
+            )
+
+        // Add the places to the ClusterManager.
+        clusterManager.addItems(places)
+        clusterManager.cluster()
+
+        // Show polygon
+        clusterManager.setOnClusterItemClickListener { item ->
+            addCircle(googleMap, item)
+            return@setOnClusterItemClickListener false
+        }
+
+        // When the camera starts moving, change the alpha value of the marker to translucent.
+        googleMap.setOnCameraMoveStartedListener {
+            clusterManager.markerCollection.markers.forEach { it.alpha = 0.3f }
+            clusterManager.clusterMarkerCollection.markers.forEach { it.alpha = 0.3f }
+        }
+
+        // Set ClusterManager as the OnCameraIdleListener so that it
+        // can re-cluster when zooming in and out.
+        googleMap.setOnCameraIdleListener {
+            // When the camera stops moving, change the alpha value back to opaque.
+            clusterManager.markerCollection.markers.forEach { it.alpha = 1.0f }
+            clusterManager.clusterMarkerCollection.markers.forEach { it.alpha = 1.0f }
+
+            // Call clusterManager.onCameraIdle() when the camera stops moving so that reclustering
+            // can be performed when the camera stops moving.
+            clusterManager.onCameraIdle()
+        }
+    }
+
+    /**
+     * Adds a [Circle] around the provided [item]
+     */
+    private fun addCircle(googleMap: GoogleMap, item: Place) {
+        circle?.remove()
+        circle = googleMap.addCircle(
+            CircleOptions()
+                .center(item.latLng)
+                .radius(1000.0)
+                .fillColor(ContextCompat.getColor(this, R.color.colorPrimaryTranslucent))
+                .strokeColor(ContextCompat.getColor(this, R.color.colorPrimary))
+        )
+    }
 }
